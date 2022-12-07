e5cb01d5abc0bbf7491427a610788e3ed560a770
Igor Encarnação Junqueira Castro <igor.castro@trescon.com.br>
2022-12-07 10:16:11 -0300

    primeiro commit

---
+++ src/test/java/com/igoreandre/apirest1/Apirest11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igoreandre.apirest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirest11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
