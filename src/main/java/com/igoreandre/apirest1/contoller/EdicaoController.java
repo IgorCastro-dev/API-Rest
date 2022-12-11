92e90cabc66ba9091ed2e367f76d5e787bac1a21
Igor Encarnação Junqueira Castro <igor.castro@trescon.com.br>
2022-12-10 21:20:34 -0300

    metodos get,put e delete de edição

--- src/main/java/com/igoreandre/apirest1/contoller/EdicaoController.java
+++ src/main/java/com/igoreandre/apirest1/contoller/EdicaoController.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,9 +33,9 @@ public class EdicaoController {
 	EdicaoService edicaoService;
 	
 	
-	@PostMapping("/cadastraredicao/{id}")
-	public ResponseEntity<Object> cadastrarediçãot(@PathVariable(value = "id") long id,@RequestBody @Valid EdicaoDTO edicaodto){
-		Optional<Evento> eventoOptional = eventoService.findById(id);
+	@PostMapping("/cadastraredicao/{id_evento}")
+	public ResponseEntity<Object> cadastrarediçãot(@PathVariable(value = "id_evento") long id_evento,@RequestBody @Valid EdicaoDTO edicaodto){
+		Optional<Evento> eventoOptional = eventoService.findById(id_evento);
 		if (!eventoOptional.isPresent()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("evento não encontrado");
 		}
@@ -50,4 +53,37 @@ public class EdicaoController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(eventoService.salvar(evento));
 		
 	}
+	
+	@PutMapping("/atualizaredicao/{id_edicao}")
+	public ResponseEntity<Object> atualizaedicao(@PathVariable(name = "id_edicao") long id_edicao,@RequestBody @Valid EdicaoDTO edicaodto){
+		Optional<Edição> edicaoOptional = edicaoService.findById(id_edicao);
+		if (!edicaoOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("edição não encontrada");
+		}
+		
+		Edição edicao = edicaoOptional.get();
+		edicao.setAno(edicaodto.getAno());
+		edicao.setCidade(edicaodto.getCidade());
+		edicao.setData_final(edicaodto.getData_final());
+		edicao.setData_inicial(edicaodto.getData_inicial());
+		edicao.setNumero(edicaodto.getNumero());
+		return ResponseEntity.status(HttpStatus.CREATED).body(edicaoService.salvar(edicao));
+		
+	}
+	
+	@DeleteMapping("deletaredicao/{id_edicao}")
+	public ResponseEntity<Object> deletaredicao(@PathVariable(name = "id_edicao") long id_edicao) {
+		Optional<Edição> edicaoOptional = edicaoService.findById(id_edicao);
+		if (!edicaoOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("edição não encontrada");
+		}
+		edicaoService.deletar(id_edicao);
+		return ResponseEntity.status(HttpStatus.OK).body("edição deletada");
+	}
+	
+	@GetMapping("/mostraredicao")
+	public ResponseEntity<Object> mostraredicao(){
+		return ResponseEntity.status(HttpStatus.OK).body(edicaoService.findAll());
+	}
+
 }
--- src/main/java/com/igoreandre/apirest1/model/services/EdicaoService.java
+++ src/main/java/com/igoreandre/apirest1/model/services/EdicaoService.java
@@ -1,8 +1,8 @@
 package com.igoreandre.apirest1.model.services;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.igoreandre.apirest1.model.entity.Edição;
 import com.igoreandre.apirest1.model.repositories.EdicaoRepository;
 
@@ -18,5 +18,18 @@ public class EdicaoService {
 	public Edição salvar(Edição edicao) {
 		return edicaoRepository.save(edicao);
 		
+	}
+
+	public Optional<Edição> findById(long id_edicao) {
+		return edicaoRepository.findById(id_edicao);
+	}
+	
+	@Transactional
+	public void deletar(long id_edicao) {
+		edicaoRepository.deleteById(id_edicao);		
+	}
+
+	public Object findAll() {
+		return edicaoRepository.findAll();
 	}
 }
