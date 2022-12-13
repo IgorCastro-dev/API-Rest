a9925405d9fc96e772976901e887363715984fda
Guilherme Lopes <guilhermelff@id.uff.br>
2022-12-13 16:37:15 -0300

    Swagger Funcionando
    troquei o springfox pelo springdoc, que é compatível com o springboot 3

--- src/main/java/com/igoreandre/apirest1/Apirest11Application.java
+++ src/main/java/com/igoreandre/apirest1/Apirest11Application.java
@@ -2,10 +2,8 @@ package com.igoreandre.apirest1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-
 public class Apirest11Application {
 
 	public static void main(String[] args) {
